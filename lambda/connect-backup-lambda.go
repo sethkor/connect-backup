35854a6b6ed1f57be2e290973967927aabec7669
Seth <18116602+sethkor@users.noreply.github.com>
2020-10-21 15:51:04 +1100

    Modify lambda to take advantage of outputing raw flows to their own prifix/file

--- lambda/connect-backup-lambda.go
+++ lambda/connect-backup-lambda.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/service/connect"
@@ -18,6 +19,7 @@ type Response struct {
 type Request struct {
 	ConnectInstanceId string `json:"ConnectInstanceId" yaml:"ConnectInstanceId"`
 	S3DestURL         string `json:"S3DestURL" yaml:"S3DestURL"`
+	FlowsRaw          *bool  `json:"FlowsRaw" yaml:"FlowsRaw"`
 }
 
 func HandleRequest(ctx context.Context, backupRequest Request) (Response, error) {
@@ -30,7 +32,7 @@ func HandleRequest(ctx context.Context, backupRequest Request) (Response, error)
 	if instanceId == "" {
 		instanceId = os.Getenv("CONNECT_INSTANCE_ID")
 		if instanceId == "" {
-			log.Fatalln("No Connect instance ID passed in either the event or as an environment variable")
+			log.Fatalln("No Connect instance ID passed in either the event or as an environment variable CONNECT_INSTANCE_ID")
 		}
 		log.Println("The ConnectInstanceId in the event was blank, using the environment var CONNECT_INSTANCE_ID")
 	}
@@ -40,21 +42,43 @@ func HandleRequest(ctx context.Context, backupRequest Request) (Response, error)
 	if s3destination == "" {
 		s3destination = os.Getenv("S3_DEST_URL")
 		if s3destination == "" {
-			log.Fatalln("No S3DestURL passed in either the event or as an environment variable")
+			log.Fatalln("No S3DestURL passed in either the event or as an environment variable S3_DEST_URL")
 		}
 		log.Println("The S3DestURL in the event was blank, using the environment var S3_DEST_URL")
 	}
+
 	s3Url, err := url.Parse(s3destination)
 	log.Println("S3 URL: " + s3Url.String())
 
 	if err != nil || s3Url.Scheme != "s3" {
 		log.Println("There was an error parsing the S3 URL")
-		return Response{Answer: "The S3 URL passed in the environment variable S3-DEST-URL was malformed"}, err
+		return Response{Answer: "The S3 URL passed in the environment variable S3_DEST_URL was malformed"}, err
 	}
+
+	var flowsRaw = false
+	if backupRequest.FlowsRaw != nil {
+		flowsRaw = *backupRequest.FlowsRaw
+	} else {
+		log.Println("The FlowsRaw in the event was blank, using the environment var FLOWS_RAW")
+
+		flowsRawEnvString := os.Getenv("FLOWS_RAW")
+		if flowsRawEnvString == "" {
+			log.Println("No FlowsRaw passed in either the event or as an environment variable FLOWS_RAW, I am setting this to false and continuing")
+		} else {
+			var err error
+			flowsRaw, err = strconv.ParseBool(flowsRawEnvString)
+			if err != nil {
+				log.Println("The FLOWS_RAW env variable was not a bool that can be parsed, I am setting this to false and continuing")
+				flowsRaw = false
+			}
+		}
+	}
+	log.Println("FlowsRaw : " + strconv.FormatBool(flowsRaw))
 
 	cb := connect_backup.ConnectBackup{ConnectInstanceId: &instanceId,
 		Svc:       svc,
 		TheWriter: &connect_backup.S3Writer{Destination: *s3Url, Sess: sess},
+		RawFlow:   flowsRaw,
 	}
 	err = cb.Backup()
 
